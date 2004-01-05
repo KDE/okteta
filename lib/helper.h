5f7ab92eca2ee3a3e227e580e806e71fe69e645f
Friedrich W. H. Kossebau <kossebau@kde.org>
2004-01-05 22:28:23 +0000

    adding missing copyright and license header
    svn path=/trunk/kdeutils/khexedit/lib/; revision=277115

--- lib/helper.h
+++ lib/helper.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+                          helper.h  -  description
+                             -------------------
+    begin                : Fri Oct 03 2003
+    copyright            : (C) 2003 by Friedrich W. H. Kossebau
+    email                : Friedrich.W.H@Kossebau.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License version 2 as published by the Free Software Foundation.       *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef KHEXEDIT_HELPER
 #define KHEXEDIT_HELPER
 
