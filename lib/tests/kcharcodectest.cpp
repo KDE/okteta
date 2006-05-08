c4f676c9b812444bd0d81fa5199455a0ab9cb8c5
Friedrich W. H. Kossebau <kossebau@kde.org>
2006-05-08 11:20:48 +0000

    importing test for kcharcodecs
    svn path=/trunk/KDE/kdeutils/khexedit/lib/; revision=538582

---
+++ lib/tests/kcharcodectest.cpp
@@ -0,0 +1,61 @@
+/***************************************************************************
+                          kcharcodectest.cpp  -  description
+                            -------------------
+    begin                : Sa Jan 15 2005
+    copyright            : (C) 2005 by Friedrich W. H. Kossebau
+    email                : Friedrich.W.H@Kossebau.de
+***************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   This library is free software; you can redistribute it and/or         *
+*   modify it under the terms of the GNU Library General Public           *
+*   License version 2 as published by the Free Software Foundation.       *
+*                                                                         *
+***************************************************************************/
+
+
+// qt specific
+#include <QtTest>
+// kde specific
+#include <kinstance.h>
+// lib specific
+#include <kcharcodec.h>
+// test specific
+#include "kcharcodectest.h"
+
+// namespaces
+using namespace KHE;
+
+
+void KCharCodecTest::initTestCase()
+{
+  Instance = new KInstance( "KTextCharCodecTest" );
+}
+
+void KCharCodecTest::cleanupTestCase()
+{
+  delete Instance;
+}
+
+//---------------------------------------------------------------------------- Tests -----
+
+
+void KCharCodecTest::testCodecNames()
+{
+  QStringList List( KCharCodec::codecNames() );
+
+  for( int i=0; i<List.size(); ++i )
+  {
+    // name exists
+    const QString &Name = List[i];
+    KCharCodec* Codec = KCharCodec::createCodec( Name );
+    QCOMPARE( Codec->name(), Name );
+    delete Codec;
+  }
+}
+
+
+QTEST_MAIN( KCharCodecTest )
+
+#include "kcharcodectest.moc"
---
+++ lib/tests/kcharcodectest.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+                          kcharcodectest.h  -  description
+                            -------------------
+    begin                : Son Mai 7 2006
+    copyright            : (C) 2006 by Friedrich W. H. Kossebau
+    email                : Friedrich.W.H@Kossebau.de
+***************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   This library is free software; you can redistribute it and/or         *
+*   modify it under the terms of the GNU Library General Public           *
+*   License version 2 as published by the Free Software Foundation.       *
+*                                                                         *
+***************************************************************************/
+
+
+
+#ifndef KCHARCODECTEST_H
+#define KCHARCODECTEST_H
+
+class KInstance;
+
+// qt specific
+#include <qobject.h>
+
+class KCharCodecTest : public QObject
+{
+  Q_OBJECT
+
+    KInstance *Instance;
+
+  private Q_SLOTS: // test functions
+    void initTestCase();
+    void cleanupTestCase();
+    void testCodecNames();
+};
+
+#endif
