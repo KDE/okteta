60e52bcab70c7573a1b64c237580cd641174f97f
Dirk Mueller <mueller@kde.org>
2006-09-03 22:46:25 +0000

    compile++
    svn path=/trunk/KDE/kdeutils/khexedit/lib/; revision=580576

--- lib/tests/kcoordrangetest.cpp
+++ lib/tests/kcoordrangetest.cpp
@@ -29,7 +29,7 @@ static const int Pos1 = 15;
 static const int Pos2 = 25;
 static const int Line1 = 10;
 static const int LineSize = 10;
-static const int Line2 = Line1 + LineSize - 1;;
+static const int Line2 = Line1 + LineSize - 1;
 static KBufferCoord Start( Pos1, Line1 );
 static KBufferCoord End(   Pos2, Line2 );
 
