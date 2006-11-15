a6560ac93be6fdaeeed24a5d388e673855826244
Friedrich W. H. Kossebau <kossebau@kde.org>
2006-11-15 18:04:39 +0000

    ... and put factory into own file, I like it so.
    svn path=/trunk/KDE/kdeutils/khexedit/parts/; revision=605172

---
+++ parts/kbytesedit/factory.cpp
@@ -0,0 +1,25 @@
+/***************************************************************************
+                          factory.cpp  -  description
+                             -------------------
+    begin                : Fri Aug 29 2003
+    copyright            : (C) 2003 by Friedrich W. H. Kossebau
+    email                : Friedrich.W.H@Kossebau.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License version 2 as published by the Free Software Foundation.       *
+ *                                                                         *
+ ***************************************************************************/
+
+
+// kde specific
+#include <kgenericfactory.h>
+// component specific
+#include "kbytearrayedit.h"
+
+
+typedef K_TYPELIST_1( KByteArrayEdit ) Product;
+K_EXPORT_COMPONENT_FACTORY( libkbytearrayedit, KGenericFactory<Product>( "kbytearrayedit" ) )
