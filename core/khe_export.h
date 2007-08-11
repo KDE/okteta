5897cf261da566a0352f26d51d1f704eafe112f4
Laurent Montel <montel@kde.org>
2007-08-11 09:50:45 +0000

    Update export symbole file to new template
    svn path=/trunk/KDE/kdeutils/khexedit/core/; revision=698859

--- core/khe_export.h
+++ core/khe_export.h
@@ -1,35 +1,55 @@
-/***************************************************************************
-                          khe_export.h  -  description
-                             -------------------
-    begin                : 22 Nov 2004
-    copyright            : (C) 2004 Dirk Mueller
-    email                : mueller@kde.org
- ***************************************************************************/
+/*  This file is part of the KDE project
+    Copyright (C) 2007 David Faure <faure@kde.org>
 
-/***************************************************************************
- *                                                                         *
- *   This library is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU Library General Public           *
- *   License version 2 as published by the Free Software Foundation.       *
- *                                                                         *
- ***************************************************************************/
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
 
 #ifndef KHE_EXPORT_H
 #define KHE_EXPORT_H
 
+/* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 
-#ifdef MAKE_OKTETACORE_LIB
-# define KHECORE_EXPORT KDE_EXPORT
-#else
-# define KHECORE_EXPORT KDE_IMPORT
+#ifndef KHECORE_EXPORT
+# if defined(MAKE_OKTETACORE_LIB)
+   /* We are building this library */ 
+#  define KHECORE_EXPORT KDE_EXPORT
+# else
+   /* We are using this library */ 
+#  define KHECORE_EXPORT KDE_IMPORT
+# endif
 #endif
 
-#ifdef MAKE_OKTETAGUI_LIB
-# define KHEUI_EXPORT KDE_EXPORT
-#else
-# define KHEUI_EXPORT KDE_IMPORT
+# ifndef KHECORE_EXPORT_DEPRECATED
+#  define KHECORE_EXPORT_DEPRECATED KDE_DEPRECATED KHECORE_EXPORT
+# endif
+
+#ifndef KHEUI_EXPORT
+# if defined(MAKE_OKTETAGUI_LIB)
+   /* We are building this library */
+#  define KHEUI_EXPORT KDE_EXPORT
+# else
+   /* We are using this library */
+#  define KHEUI_EXPORT KDE_IMPORT
+# endif
 #endif
+
+# ifndef KHEUI_EXPORT_DEPRECATED
+#  define KHEUI_EXPORT_DEPRECATED KDE_DEPRECATED KHEUI_EXPORT
+# endif
+
 
 #endif
