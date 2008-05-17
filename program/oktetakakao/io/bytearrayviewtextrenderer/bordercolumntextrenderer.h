7fc847430b99c95cf05a12dbb7de1eb92d185234
Friedrich W. H. Kossebau <kossebau@kde.org>
2008-05-17 21:33:23 +0000

    krazy fix: misspelled include guards
    svn path=/trunk/KDE/kdeutils/okteta/; revision=808930

--- program/oktetakakao/io/bytearrayviewtextrenderer/bordercolumntextrenderer.h
+++ program/oktetakakao/io/bytearrayviewtextrenderer/bordercolumntextrenderer.h
@@ -20,8 +20,8 @@
     License along with this library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef BORDERCOLUMNREXTRENDERER_H
-#define BORDERCOLUMNREXTRENDERER_H
+#ifndef BORDERCOLUMNTEXTRENDERER_H
+#define BORDERCOLUMNTEXTRENDERER_H
 
 
 // lib
