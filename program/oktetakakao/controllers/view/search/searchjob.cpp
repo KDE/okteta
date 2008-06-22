9ebe002b8a7b626a35700de433e40960206b46a6
Friedrich W. H. Kossebau <kossebau@kde.org>
2008-06-22 00:02:00 +0000

    added: class SearchJob (primitive gui-updating version for now)
    svn path=/trunk/KDE/kdeutils/okteta/; revision=822915

---
+++ program/oktetakakao/controllers/view/search/searchjob.cpp
@@ -0,0 +1,52 @@
+/*
+    This file is part of the Okteta Kakao module, part of the KDE project.
+
+    Copyright 2008 Friedrich W. H. Kossebau <kossebau@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "searchjob.h"
+
+// filter
+#include <abstractbytearrayfilter.h>
+// Okteta core
+#include <kabstractbytearraymodel.h>
+// Qt
+#include <QtGui/QApplication>
+
+static const int MaxEventProcessTimeInMS = 100;
+
+int SearchJob::exec()
+{
+    //TODO: what kind of signal could a filter send?
+    connect( mByteArrayModel, SIGNAL(searchedBytes(int)), SLOT(onBytesSearched()) );
+
+    // TODO: support ignorecase
+    const int result = mFindForward ?
+        mByteArrayModel->indexOf( mSearchData, mStartIndex ) :
+        mByteArrayModel->lastIndexOf( mSearchData, mStartIndex-mSearchData.size()+1 );
+
+    deleteLater(); // TODO: could be reused on next search
+
+    return result;
+}
+
+void SearchJob::onBytesSearched()
+{
+    QApplication::processEvents( QEventLoop::ExcludeUserInputEvents | QEventLoop::ExcludeSocketNotifiers, MaxEventProcessTimeInMS );
+}
---
+++ program/oktetakakao/controllers/view/search/searchjob.h
@@ -0,0 +1,61 @@
+/*
+    This file is part of the Okteta Kakao module, part of the KDE project.
+
+    Copyright 2008 Friedrich W. H. Kossebau <kossebau@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SEARCHJOB_H
+#define SEARCHJOB_H
+
+// Qt
+#include <QtCore/QObject>
+#include <QtCore/QByteArray>
+
+namespace KHECore {
+class KAbstractByteArrayModel;
+}
+
+class SearchJob : public QObject // not yet: KJob
+{
+  Q_OBJECT
+
+  public:
+    SearchJob( const KHECore::KAbstractByteArrayModel *model,
+               const QByteArray &searchData, int startIndex, bool findForward );
+
+  public:
+    int exec();
+
+  protected Q_SLOTS:
+    void onBytesSearched();
+
+  protected:
+    const KHECore::KAbstractByteArrayModel *mByteArrayModel;
+
+    const QByteArray mSearchData;
+    int mStartIndex;
+    bool mFindForward;
+};
+
+inline SearchJob::SearchJob( const KHECore::KAbstractByteArrayModel *model,
+               const QByteArray &searchData, int startIndex, bool findForward )
+ : mByteArrayModel( model ), mSearchData( searchData ), mStartIndex( startIndex), mFindForward( findForward )
+{}
+
+#endif
