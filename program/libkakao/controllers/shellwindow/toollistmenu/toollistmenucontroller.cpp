4b4c68887b73ec57a286360dcde381d77c46ff51
Friedrich W. H. Kossebau <kossebau@kde.org>
2008-08-02 10:16:08 +0000

    added: controller for tools
    svn path=/trunk/KDE/kdeutils/okteta/; revision=840947

---
+++ program/libkakao/controllers/shellwindow/toollistmenu/toollistmenucontroller.cpp
@@ -0,0 +1,67 @@
+/*
+    This file is part of the Kakao Framework, part of the KDE project.
+
+    Copyright 2008 Friedrich W. H. Kossebau <kossebau@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "toollistmenucontroller.h"
+
+// lib
+#include <iwidgetsdockable.h>
+// KDE
+#include <KXmlGuiWindow>
+#include <KXMLGUIFactory>
+// Qt
+#include <QtGui/QDockWidget>
+#include <QtGui/QAction>
+
+
+static const char ToolListActionListId[] = "tools_list";
+
+ToolListMenuController::ToolListMenuController( KDE::If::WidgetsDockable *widgetsDockable,
+                                      KXmlGuiWindow *window )
+ : mWidgetsDockable( widgetsDockable ), mWindow( window )
+{
+    // TODO: for now this is only called on start, so first create all tools/views before this controller
+    updateActions();
+}
+
+void ToolListMenuController::setView( KAbstractView *view )
+{
+Q_UNUSED(view)
+}
+
+void ToolListMenuController::updateActions()
+{
+    mWindow->unplugActionList( ToolListActionListId );
+
+    qDeleteAll( mToolActionList );
+    mToolActionList.clear();
+
+    const QList<QDockWidget*> dockWidgets = mWidgetsDockable->dockWidgets();
+
+    foreach( const QDockWidget* dockWidget, dockWidgets )
+    {
+        QAction *action = dockWidget->toggleViewAction();
+        action->setText( dockWidget->windowTitle() );
+//         action->setText( mToolView->title() );
+        mToolActionList.append( action );
+    }
+    mWindow->plugActionList( ToolListActionListId, mToolActionList );
+}
