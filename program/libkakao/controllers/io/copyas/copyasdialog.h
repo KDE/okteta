4bb059b2e6b63a830089164f37396178a663e0ae
Friedrich W. H. Kossebau <kossebau@kde.org>
2008-11-17 16:09:52 +0000

    fixed: correct include guard names
    svn path=/trunk/KDE/kdeutils/okteta/; revision=885648

--- program/libkakao/controllers/io/copyas/copyasdialog.h
+++ program/libkakao/controllers/io/copyas/copyasdialog.h
@@ -20,8 +20,8 @@
     License along with this library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef KGOTOOFFSETDIALOG_H
-#define KGOTOOFFSETDIALOG_H
+#ifndef COPYASDIALOG_H
+#define COPYASDIALOG_H
 
 // KDE
 #include <KDialog>
