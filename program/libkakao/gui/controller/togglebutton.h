3c51643d041caf0eb32a810ffc9513f243b29e02
Friedrich W. H. Kossebau <kossebau@kde.org>
2009-01-08 00:02:09 +0000

    added: a toggle button, e.g. to be used in the status bar
    svn path=/trunk/KDE/kdeutils/okteta/; revision=907399

---
+++ program/libkakao/gui/controller/togglebutton.h
@@ -0,0 +1,49 @@
+/*
+    This file is part of the Kakao Framework, part of the KDE project.
+
+    Copyright 2009 Friedrich W. H. Kossebau <kossebau@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TOGGLEBUTTON_H
+#define TOGGLEBUTTON_H
+
+// Qt
+#include <QtGui/QToolButton>
+
+class KIcon;
+
+class ToggleButtonPrivate;
+
+class ToggleButton : public QToolButton
+{
+    Q_OBJECT
+
+  public:
+    ToggleButton( const KIcon& icon, const QString& toolTip, QWidget* parent = 0 );
+    virtual ~ToggleButton();
+
+  public:
+    void setCheckedState( const KIcon& icon, const QString& toolTip );
+
+  private:
+    ToggleButtonPrivate* const d;
+    Q_PRIVATE_SLOT( d, void onToggled( bool ) )
+};
+
+#endif
