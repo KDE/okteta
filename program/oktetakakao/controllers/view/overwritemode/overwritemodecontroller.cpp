c459496bbde7362898fdab6374c5311f737cdbc1
Friedrich W. H. Kossebau <kossebau@kde.org>
2009-01-15 00:19:51 +0000

    fixed: catch change of independent overwriteMode
    svn path=/trunk/KDE/kdeutils/okteta/; revision=911181

--- program/oktetakakao/controllers/view/overwritemode/overwritemodecontroller.cpp
+++ program/oktetakakao/controllers/view/overwritemode/overwritemodecontroller.cpp
@@ -1,7 +1,7 @@
 /*
     This file is part of the Okteta Kakao module, part of the KDE project.
 
-    Copyright 2008 Friedrich W. H. Kossebau <kossebau@kde.org>
+    Copyright 2008-2009 Friedrich W. H. Kossebau <kossebau@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -50,7 +50,7 @@ OverwriteModeController::OverwriteModeController( KXMLGUIClient* guiClient )
 
 void OverwriteModeController::setTargetModel( AbstractModel* model )
 {
-//     if( mByteArrayDisplay ) mByteArrayDisplay->disconnect( mSetOverWriteAction );
+    if( mByteArrayDisplay ) mByteArrayDisplay->disconnect( mSetOverWriteAction );
 
     mByteArrayDisplay = model ? model->findBaseModel<KByteArrayDisplay*>() : 0;
 
@@ -58,8 +58,8 @@ void OverwriteModeController::setTargetModel( AbstractModel* model )
     {
         mSetOverWriteAction->setChecked( mByteArrayDisplay->isOverwriteMode() );
 
-//         connect( mByteArrayDisplay, SIGNAL(overwriteModeChanged( bool )),
-//                  mSetOverWriteAction, SLOT(setChecked( bool )) );
+        connect( mByteArrayDisplay, SIGNAL(overwriteModeChanged( bool )),
+                 mSetOverWriteAction, SLOT(setChecked( bool )) );
         // TODO: catch if isOverwriteOnly changes
     }
 
