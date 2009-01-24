bb8b4579ca05c0fb64ce6d222a2b7b88b9e43b22
Friedrich W. H. Kossebau <kossebau@kde.org>
2009-01-24 23:35:20 +0000

    added: a property name
    svn path=/trunk/KDE/kdeutils/okteta/; revision=916263

--- core/bookmark.h
+++ core/bookmark.h
@@ -1,7 +1,7 @@
 /*
     This file is part of the Okteta Core library, part of the KDE project.
 
-    Copyright 2007 Friedrich W. H. Kossebau <kossebau@kde.org>
+    Copyright 2007,2009 Friedrich W. H. Kossebau <kossebau@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -25,6 +25,8 @@
 
 // lib
 #include "oktetacore_export.h"
+// Qt
+#include <QtCore/QString>
 
 
 namespace KHECore
@@ -43,19 +45,26 @@ class OKTETACORE_EXPORT Bookmark
     bool operator==( const Bookmark &other ) const;
   public:
     int offset() const;
+    QString name() const;
     bool isValid() const;
   public:
     void move( int offset );
+    void setName( const QString& name );
   protected:
     int mOffset;
+    QString mName;
 };
+
 
 inline Bookmark::Bookmark( int offset ) : mOffset( offset ) {}
 inline Bookmark::Bookmark() : mOffset( InvalidOffset ) {}
-inline bool Bookmark::operator==( const Bookmark &other ) const { return mOffset == other.mOffset; }
+inline bool Bookmark::operator==( const Bookmark &other ) const { return mOffset == other.mOffset; } 
 inline bool Bookmark::isValid() const { return mOffset != InvalidOffset; }
 inline int Bookmark::offset() const { return mOffset; }
+inline QString Bookmark::name() const { return mName; }
+
 inline void Bookmark::move( int offset ) { mOffset += offset; }
+inline void Bookmark::setName( const QString& name ) { mName = name; }
 
 }
 
