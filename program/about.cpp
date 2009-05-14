8f655a0fc4eca44d55e830e14504fd3331641214
Friedrich W. H. Kossebau <kossebau@kde.org>
2009-05-14 22:50:23 +0000

    changed: set version to 0.3.0, should be done all
    svn path=/trunk/KDE/kdeutils/okteta/; revision=968111

--- program/about.cpp
+++ program/about.cpp
@@ -30,7 +30,7 @@
 
 // Program
 static const char ProgramId[] =          "okteta";
-static const char ProgramVersion[] =     "0.2.80";
+static const char ProgramVersion[] =     "0.3.0";
 static const char ProgramHomepage[] =    "http://utils.kde.org/projects/okteta";
 // Author
 static const char FWHKEmailAddress[] =   "kossebau@kde.org";
