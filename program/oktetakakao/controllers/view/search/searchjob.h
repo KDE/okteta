1f2923e74b3bb78b54656b0ebcca54212d5ea567
Friedrich W. H. Kossebau <kossebau@kde.org>
2009-07-03 11:14:59 +0000

    fixed: search/replace did not respect ignorecase (backport of #990799)
    svn path=/branches/KDE/4.3/kdeutils/okteta/; revision=990805

--- program/oktetakakao/controllers/view/search/searchjob.h
+++ program/oktetakakao/controllers/view/search/searchjob.h
@@ -1,7 +1,7 @@
 /*
     This file is part of the Okteta Kakao module, part of the KDE project.
 
-    Copyright 2008 Friedrich W. H. Kossebau <kossebau@kde.org>
+    Copyright 2008-2009 Friedrich W. H. Kossebau <kossebau@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -29,6 +29,7 @@
 
 namespace KHECore {
 class AbstractByteArrayModel;
+class CharCodec;
 }
 
 
@@ -38,10 +39,15 @@ class SearchJob : public QObject // not yet: KJob
 
   public:
     SearchJob( const KHECore::AbstractByteArrayModel *model,
-               const QByteArray &searchData, int startIndex, bool findForward );
+               const QByteArray &searchData, int startIndex, bool findForward, bool ignoreCase, const QString& charCodecName );
+    virtual ~SearchJob();
 
   public:
     int exec();
+
+  protected:
+    int indexOfIgnoreCase();
+    int lastIndexOfIgnoreCase();
 
   protected Q_SLOTS:
     void onBytesSearched();
@@ -49,14 +55,11 @@ class SearchJob : public QObject // not yet: KJob
   protected:
     const KHECore::AbstractByteArrayModel *mByteArrayModel;
 
-    const QByteArray mSearchData;
+    QByteArray mSearchData;
     int mStartIndex;
     bool mFindForward;
+    bool mIgnoreCase;
+    const KHECore::CharCodec* mCharCodec;
 };
-
-inline SearchJob::SearchJob( const KHECore::AbstractByteArrayModel *model,
-               const QByteArray &searchData, int startIndex, bool findForward )
- : mByteArrayModel( model ), mSearchData( searchData ), mStartIndex( startIndex), mFindForward( findForward )
-{}
 
 #endif
