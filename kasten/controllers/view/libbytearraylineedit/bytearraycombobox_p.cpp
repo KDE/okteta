facf38bdabf74eef4462d497dc27461ffb2e94bf
Friedrich W. H. Kossebau <kossebau@kde.org>
2009-09-18 00:17:24 +0000

    renamed: KByteArrayLineEdit is now ByteArrayComboBox
    svn path=/trunk/KDE/kdeutils/okteta/; revision=1025094

---
+++ kasten/controllers/view/libbytearraylineedit/bytearraycombobox_p.cpp
@@ -0,0 +1,99 @@
+/*
+    This file is part of the Okteta Kasten module, part of the KDE project.
+
+    Copyright 2009 Friedrich W. H. Kossebau <kossebau@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "bytearraycombobox_p.h"
+#include "bytearraycombobox.h"
+
+// lib
+#include "kbytearrayvalidator.h"
+// KDE
+#include <KLineEdit>
+// Qt
+#include <QtGui/QLayout>
+#include <QtGui/QAbstractItemView>
+
+
+void ByteArrayComboBoxPrivate::init()
+{
+    Q_Q( ByteArrayComboBox );
+
+    QVBoxLayout *baseLayout = new QVBoxLayout( q );
+    baseLayout->setMargin( 0 );
+
+    mFormatComboBox = new KComboBox( q );
+    mFormatComboBox->addItems( KByteArrayValidator::codecNames() );
+    q->connect( mFormatComboBox, SIGNAL(activated(int)), SLOT(onFormatChanged(int)) );
+
+    mDataEdit = new KLineEdit( q );
+    q->setFocusProxy( mDataEdit );
+    q->connect( mDataEdit, SIGNAL(textChanged(const QString&)), SLOT(onDataChanged(const QString&)) );
+    QAbstractItemView* formatComboBoxListView = mFormatComboBox->view();
+    QObject::connect( formatComboBoxListView, SIGNAL(activated( const QModelIndex& )),
+             mDataEdit, SLOT(setFocus()) );
+    // TODO: is a workaround for Qt 4.5.1 which doesn't emit activated() for mouse clicks
+    QObject::connect( formatComboBoxListView, SIGNAL(pressed( const QModelIndex& )),
+             mDataEdit, SLOT(setFocus()) );
+    mValidator = new KByteArrayValidator( mDataEdit );
+    mDataEdit->setValidator( mValidator );
+
+    baseLayout->addWidget( mFormatComboBox );
+    baseLayout->addWidget( mDataEdit );
+    q->setTabOrder( mFormatComboBox, mDataEdit );
+
+    onFormatChanged( mFormatComboBox->currentIndex() );
+}
+
+void ByteArrayComboBoxPrivate::setCharCodec( const QString& charCodecName )
+{
+    // update the char string
+    const QByteArray currentData = mValidator->toByteArray( mData[KByteArrayValidator::CharCoding] );
+
+    mValidator->setCharCodec( charCodecName );
+
+    const QString dataString = mValidator->toString( currentData );
+    mData[KByteArrayValidator::CharCoding] = dataString;
+
+    const bool isCharVisible = ( mFormatComboBox->currentIndex() == KByteArrayValidator::CharCoding );
+
+    if( isCharVisible )
+        mDataEdit->setText( dataString );
+}
+
+
+void ByteArrayComboBoxPrivate::onFormatChanged( int index )
+{
+    Q_Q( ByteArrayComboBox );
+
+    mValidator->setCodec( (KByteArrayValidator::Coding)index );
+    mDataEdit->setText( mData[index] );
+
+    emit q->formatChanged( index );
+}
+
+void ByteArrayComboBoxPrivate::onDataChanged( const QString& data )
+{
+    Q_Q( ByteArrayComboBox );
+
+    mData[mFormatComboBox->currentIndex()] = data;
+
+    emit q->dataChanged( mValidator->toByteArray(data) );
+}
---
+++ kasten/controllers/view/libbytearraylineedit/bytearraycombobox_p.h
@@ -0,0 +1,79 @@
+/*
+    This file is part of the Okteta Kasten module, part of the KDE project.
+
+    Copyright 2009 Friedrich W. H. Kossebau <kossebau@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BYTEARRAYCOMBOBOX_P_H
+#define BYTEARRAYCOMBOBOX_P_H
+
+// lib
+#include "bytearraycombobox.h"
+#include "kbytearrayvalidator.h"
+// KDE
+#include <KComboBox>
+// Qt
+#include <QtCore/QByteArray>
+#include <QtCore/QString>
+
+class KLineEdit;
+
+
+class ByteArrayComboBoxPrivate
+{
+  public:
+    explicit ByteArrayComboBoxPrivate( ByteArrayComboBox* parent );
+
+  public:
+    QByteArray data() const;
+    int format() const;
+
+  public:
+    void init();
+    void setCharCodec( const QString& charCodecName );
+
+    void onFormatChanged( int index );
+    void onDataChanged( const QString &data );
+
+  protected:
+    ByteArrayComboBox* const q_ptr;
+    Q_DECLARE_PUBLIC( ByteArrayComboBox )
+
+    KComboBox* mFormatComboBox;
+    KLineEdit* mDataEdit;
+
+    QString mData[KByteArrayValidator::CodecNumber];
+    KByteArrayValidator* mValidator;
+};
+
+
+inline ByteArrayComboBoxPrivate::ByteArrayComboBoxPrivate( ByteArrayComboBox* parent )
+  : q_ptr( parent )
+{}
+
+inline QByteArray ByteArrayComboBoxPrivate::data() const
+{
+    return mValidator->toByteArray( mData[mFormatComboBox->currentIndex()] );
+}
+inline int ByteArrayComboBoxPrivate::format() const
+{
+    return mFormatComboBox->currentIndex();
+}
+
+#endif
