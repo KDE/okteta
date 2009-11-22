06c1fcee4713f3ef4653f8bb5aa73222fa0127ee
Alexander Richardson <arichardson.kde@googlemail.com>
2009-11-22 14:29:07 +0000

    fix typo in filename
    svn path=/trunk/KDE/kdeutils/okteta/; revision=1052799

--- kasten/controllers/view/structures/datatypes/dynamiclengtharraydatainformation.cpp
+++ kasten/controllers/view/structures/datatypes/dynamiclengtharraydatainformation.cpp
@@ -20,7 +20,7 @@
  *   License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "dynamiclenghtarraydatainformation.h"
+#include "dynamiclengtharraydatainformation.h"
 #include "primitivedatainformation.h"
 
 void DynamicLengthArrayDataInformation::resizeChildren()
