180241018bb8fe47b4c0cebb7178be87ea786117
Friedrich W. H. Kossebau <kossebau@kde.org>
2009-11-26 03:44:45 +0000

    added: are now ui and kcfg files around; also ignore template folders
    svn path=/trunk/KDE/kdeutils/okteta/; revision=1054472

--- kasten/Messages.sh
+++ kasten/Messages.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-$XGETTEXT `find . -name \*.cpp` -o $podir/liboktetakasten.pot
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -type d -name template -prune -o -name \*.cpp -print` -o $podir/liboktetakasten.pot
