9163a308e0871082b16a32c3d8911b52bfc07bc8
Friedrich W. H. Kossebau <kossebau@kde.org>
2009-12-03 12:00:13 +0000

    fixed: needs brackets around "-name \*.cpp -o -name \*.h", otherwise now only headers will be returned
    svn path=/trunk/KDE/kdeutils/okteta/; revision=1057847

--- kasten/Messages.sh
+++ kasten/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -type d -name template -prune -o -name \*.cpp -o -name \*.h -print` -o $podir/liboktetakasten.pot
+$XGETTEXT `find . -type d -name template -prune -o \( -name \*.cpp -o -name \*.h \) -print` -o $podir/liboktetakasten.pot
