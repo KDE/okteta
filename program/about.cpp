50f6a12c480ff2d82c7469a1b25c7505dc94f9cb
Friedrich W. H. Kossebau <kossebau@kde.org>
2010-02-01 18:18:15 +0000

    changed: update version for next release which will the final 4.4.0
    svn path=/branches/KDE/4.4/kdeutils/okteta/; revision=1083644

--- program/about.cpp
+++ program/about.cpp
@@ -28,7 +28,7 @@
 
 // Program
 static const char ProgramId[] =          "okteta";
-static const char ProgramVersion[] =     "0.3.93";
+static const char ProgramVersion[] =     "0.4.0";
 static const char ProgramHomepage[] =    "http://utils.kde.org/projects/okteta";
 // Author
 static const char FWHKEmailAddress[] =   "kossebau@kde.org";
