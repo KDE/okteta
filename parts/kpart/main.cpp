f54fadfee74e3def6a76463a4df93c8eb626b6a1
Friedrich W. H. Kossebau <kossebau@kde.org>
2010-11-20 16:04:10 +0000

    changed: ported away from deprecated KGenericFactory
    svn path=/trunk/KDE/kdesdk/okteta/; revision=1199095

--- parts/kpart/main.cpp
+++ parts/kpart/main.cpp
@@ -1,7 +1,7 @@
 /*
     This file is part of the Okteta KPart module, part of the KDE project.
 
-    Copyright 2009 Friedrich W. H. Kossebau <kossebau@kde.org>
+    Copyright 2009-2010 Friedrich W. H. Kossebau <kossebau@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -23,4 +23,4 @@
 #include "partfactory.h"
 
 
-K_EXPORT_COMPONENT_FACTORY( oktetapart, OktetaPartFactory )
+K_EXPORT_PLUGIN( OktetaPartFactory )
