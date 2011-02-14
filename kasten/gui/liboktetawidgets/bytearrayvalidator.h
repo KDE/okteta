6f107c04647a9aeb736b56cf25b301109c9aa426
Friedrich W. H. Kossebau <kossebau@kde.org>
2011-02-14 23:50:07 +0000

    added: properties minLength and maxLength, to control length of edited bytearray
    svn path=/trunk/KDE/kdesdk/okteta/; revision=1220737

--- kasten/gui/liboktetawidgets/bytearrayvalidator.h
+++ kasten/gui/liboktetawidgets/bytearrayvalidator.h
@@ -1,7 +1,7 @@
 /*
-    This file is part of the Okteta Kasten module, part of the KDE project.
+    This file is part of the Okteta Kasten module, made within the KDE community.
 
-    Copyright 2006,2009 Friedrich W. H. Kossebau <kossebau@kde.org>
+    Copyright 2006,2009,2011 Friedrich W. H. Kossebau <kossebau@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -41,10 +41,20 @@ class ByteArrayValidator : public QValidator
 
   public:
     // matching Okteta::ValueCoding
-    enum Coding { InvalidCoding = -1, HexadecimalCoding=0, DecimalCoding=1, OctalCoding=2, BinaryCoding=3, CharCoding=4, Utf8Coding=5 };
+    enum Coding {
+        InvalidCoding = -1,
+        HexadecimalCoding = 0,
+        DecimalCoding = 1,
+        OctalCoding = 2,
+        BinaryCoding = 3,
+        CharCoding = 4,
+        Utf8Coding = 5
+    };
 
   public:
-    explicit ByteArrayValidator( QObject* parent, Coding codecId = CharCoding, int charCodecId = Okteta::LocalEncoding );
+    explicit ByteArrayValidator( QObject* parent = 0,
+                                 Coding codecId = CharCoding,
+                                 int charCodecId = LocalEncoding );
 
     virtual ~ByteArrayValidator();
 
@@ -52,10 +62,20 @@ class ByteArrayValidator : public QValidator
     virtual QValidator::State validate( QString& input, int& pos ) const;
 
   public:
-    /// sets one of the value codecs or any char codec
+    /// Sets one of the value codecs or the current char codec.
     void setCodec( Coding codecId );
-    /// sets the char codec to use
+    /// Sets the char codec to use. Does not change the current codec.
     void setCharCodec( const QString& charCodecName );
+    /// Sets the maximal length of the edited bytearray to @p maxLength.
+    /// If @p maxLength is negative, the behaviour is undefined. Default is @c 32767.
+    void setMaxLength( int maxLength );
+    /// Sets the maximal length of the edited bytearray to @p minLength.
+    /// If @p minLength is negative, the behaviour is undefined. Default is @c 0.
+    void setMinLength( int minLength );
+
+  public:
+    int maxLength() const;
+    int minLength() const;
 
   public:
     QByteArray toByteArray( const QString& string ) const;
@@ -73,7 +93,13 @@ class ByteArrayValidator : public QValidator
     Coding mCodecId;
     ValueCodec* mValueCodec;
     CharCodec* mCharCodec;
+    int mMaxLength;
+    int mMinLength;
 };
+
+
+inline int ByteArrayValidator::maxLength() const { return mMaxLength; }
+inline int ByteArrayValidator::minLength() const { return mMinLength; }
 
 }
 
