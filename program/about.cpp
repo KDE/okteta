2895a6ea38e51ff08761b653c5aa0a060f2d0f3b
Friedrich W. H. Kossebau <kossebau@kde.org>
2011-07-23 15:13:36 +0000

    bumped version to 0.7.1
    svn path=/branches/KDE/4.7/kdesdk/okteta/; revision=1242845

--- program/about.cpp
+++ program/about.cpp
@@ -1,7 +1,7 @@
 /*
     This file is part of the Okteta program, made within the KDE community.
 
-    Copyright 2006-2010 Friedrich W. H. Kossebau <kossebau@kde.org>
+    Copyright 2006-2011 Friedrich W. H. Kossebau <kossebau@kde.org>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
@@ -28,7 +28,7 @@
 
 // Program
 static const char ProgramId[] =          "okteta";
-static const char ProgramVersion[] =     "0.7.0";
+static const char ProgramVersion[] =     "0.7.1";
 static const char ProgramHomepage[] =    "http://utils.kde.org/projects/okteta";
 // Author
 static const char FWHKEmailAddress[] =   "kossebau@kde.org";
