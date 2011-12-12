f3c577fd3a172c1403918085fdd04b5c8905c5d6
Friedrich W. H. Kossebau <kossebau@kde.org>
2011-12-12 19:34:52 +0000

    Changed: bump version number for RC1
    svn path=/trunk/KDE/kdesdk/okteta/; revision=1268398

--- program/about.cpp
+++ program/about.cpp
@@ -28,7 +28,7 @@
 
 // Program
 static const char ProgramId[] =          "okteta";
-static const char ProgramVersion[] =     "0.7.60";
+static const char ProgramVersion[] =     "0.7.90";
 static const char ProgramHomepage[] =    "http://utils.kde.org/projects/okteta";
 // Author
 static const char FWHKEmailAddress[] =   "kossebau@kde.org";
