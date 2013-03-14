50744b7bcd7f752d6d06bcf2a357eee192ae166e
Alex Richardson <arichardson.kde@googlemail.com>
2013-03-14 23:24:38 +0100

    GIT_SILENT fix narrowing conversion warning

--- kasten/controllers/test/customtostringtest.cpp
+++ kasten/controllers/test/customtostringtest.cpp
@@ -42,17 +42,17 @@ void CustomToStringTest::initTestCase()
     QVERIFY(KGlobal::dirs()->addResourceDir("data", QLatin1String(SRCDIR "/resources")));
 }
 
-static char uuid1[16] =
+static uchar uuid1[16] =
 {
     0x55,0x0e,0x84,0x00,0xe2,0x9b,0x41,0xd4,0xa7,0x16,0x44,0x66,0x55,0x44,0x00,0x00
 };
 
-static char uuid2[16] =
+static uchar uuid2[16] =
 {
     0x3f,0x25,0x04,0xe0,0x4f,0x89,0x11,0xd3,0x9a,0x0c,0x03,0x05,0xe8,0x2c,0x33,0x01
 };
 
-static char nullUuid[16] =
+static uchar nullUuid[16] =
 {
     0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0
 };
@@ -61,15 +61,22 @@ void CustomToStringTest::testUuid_data()
 {
     QTest::addColumn<bool>("isGUID");
     QTest::addColumn<QString>("uuidString");
-    QTest::addColumn<QByteArray>("data");
-    QTest::newRow("uuid1") << false << "{550e8400-e29b-41d4-a716-446655440000}" << QByteArray::fromRawData(uuid1, 16); //from wikipedia
-    QTest::newRow("uuid2") << false << "{3f2504e0-4f89-11d3-9a0c-0305e82c3301}"<< QByteArray::fromRawData(uuid2, 16); //from wikipedia
-    QTest::newRow("null uuid") << false << "{00000000-0000-0000-0000-000000000000}"<< QByteArray::fromRawData(nullUuid, 16);
+    QTest::addColumn<void*>("data");
+    QTest::newRow("uuid1") << false << "{550e8400-e29b-41d4-a716-446655440000}"
+        << QByteArray::fromRawData(reinterpret_cast<char*>(uuid1), sizeof(uuid1));
+    QTest::newRow("uuid2") << false << "{3f2504e0-4f89-11d3-9a0c-0305e82c3301}"
+        << QByteArray::fromRawData(reinterpret_cast<char*>(uuid2), sizeof(uuid2));
+    QTest::newRow("null uuid") << false << "{00000000-0000-0000-0000-000000000000}"
+        << QByteArray::fromRawData(reinterpret_cast<char*>(nullUuid), sizeof(nullUuid));
 
     //now the same just as a Microsoft GUID
-    QTest::newRow("guid1") << true << "{00840e55-9be2-d441-a716-446655440000}" << QByteArray::fromRawData(uuid1, 16); //from wikipedia
-    QTest::newRow("guid2") << true << "{e004253f-894f-d311-9a0c-0305e82c3301}"<< QByteArray::fromRawData(uuid2, 16); //from wikipedia
-    QTest::newRow("null guid") << true << "{00000000-0000-0000-0000-000000000000}"<< QByteArray::fromRawData(nullUuid, 16);
+    QTest::newRow("guid1") << true << "{00840e55-9be2-d441-a716-446655440000}"
+        << QByteArray::fromRawData(reinterpret_cast<char*>(uuid1), sizeof(uuid1));
+    QTest::newRow("guid2") << true << "{e004253f-894f-d311-9a0c-0305e82c3301}"
+        << QByteArray::fromRawData(reinterpret_cast<char*>(uuid2), sizeof(uuid2));
+    QTest::newRow("null guid") << true << "{00000000-0000-0000-0000-000000000000}"
+        << QByteArray::fromRawData(reinterpret_cast<char*>(nullUuid), sizeof(nullUuid));
+
 }
 
 void CustomToStringTest::testUuid()
