45c75b1cc1a9db4b222b6e8f893e69f7f9a14798
Friedrich W. H. Kossebau <kossebau@kde.org>
2013-06-16 00:16:46 +0200

    Bump version to 0.10.85

--- program/about.cpp
+++ program/about.cpp
@@ -28,7 +28,7 @@
 
 // Program
 static const char ProgramId[] =          "okteta";
-static const char ProgramVersion[] =     "0.10.60";
+static const char ProgramVersion[] =     "0.10.85";
 static const char ProgramHomepage[] =    "http://userbase.kde.org/Okteta";
 // Author
 static const char FWHKEmailAddress[] =   "kossebau@kde.org";
