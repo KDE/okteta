5076579b8695f0466d321ab764cff8f2492e6f32
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-11-20 19:30:48 +0100

    Bump version to 0.23.90
    GIT_SILENT

--- program/about.cpp
+++ program/about.cpp
@@ -29,7 +29,7 @@
 OktetaAboutData::OktetaAboutData()
 : KAboutData( QStringLiteral("okteta"),
               i18n("Okteta"), // name
-              QStringLiteral("0.23.80"),
+              QStringLiteral("0.23.90"),
               i18n("Hex editor"), // description
               KAboutLicense::GPL_V2,
               i18n("Copyright 2006-2017 Friedrich W. H. Kossebau"), //copyright
