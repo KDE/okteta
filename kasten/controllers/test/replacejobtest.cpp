3ceaa253b7d894446cef45db66b115c8b97194c1
Friedrich W. H. Kossebau <kossebau@kde.org>
2019-06-08 13:38:19 +0200

    ReplaceJobTest: extend test with TestReplaceUserQueryAgent variant

--- kasten/controllers/test/replacejobtest.cpp
+++ kasten/controllers/test/replacejobtest.cpp
@@ -24,6 +24,7 @@
 
 // test object
 #include "../view/replace/replacejob.hpp"
+#include "../view/replace/replaceuserqueryable.hpp"
 // Okteta Kasten gui
 #include <Kasten/Okteta/ByteArrayView>
 // Okteta Kasten core
@@ -33,6 +34,52 @@
 // Qt
 #include <QTest>
 #include <QSignalSpy>
+
+
+Q_DECLARE_METATYPE(QVector<Kasten::ReplaceBehaviour>)
+
+class TestReplaceUserQueryAgent : public QObject
+                                , public Kasten::If::ReplaceUserQueryable
+{
+    Q_OBJECT
+    Q_INTERFACES(
+        Kasten::If::ReplaceUserQueryable
+    )
+
+public:
+    explicit TestReplaceUserQueryAgent(const QVector<Kasten::ReplaceBehaviour>& replies)
+        : m_replies(replies)
+    {}
+
+public:
+    int noOfReplacements() const { return m_noOfReplacements; }
+
+public: // If::ReplaceUserQueryable API
+    void queryContinue(Kasten::FindDirection direction, int noOfReplacements) override;
+    void queryReplaceCurrent() override;
+
+Q_SIGNALS: // If::ReplaceUserQueryable API
+    void queryContinueFinished(bool result) override;
+    void queryReplaceCurrentFinished(Kasten::ReplaceBehaviour result) override;
+
+private:
+    QVector<Kasten::ReplaceBehaviour> m_replies;
+    int m_noOfReplacements = 0;
+};
+
+void TestReplaceUserQueryAgent::queryContinue(Kasten::FindDirection direction, int noOfReplacements)
+{
+    Q_UNUSED(direction);
+    m_noOfReplacements = noOfReplacements;
+    return queryContinueFinished(true);
+}
+
+void TestReplaceUserQueryAgent::queryReplaceCurrent()
+{
+    Q_ASSERT(!m_replies.isEmpty());
+    return queryReplaceCurrentFinished(m_replies.takeFirst());
+}
+
 
 void ReplaceJobTest::compare(const Okteta::PieceTableByteArrayModel* byteArrayModel,
                              const QByteArray& expectedData)
@@ -57,6 +104,7 @@ void ReplaceJobTest::testReplace_data()
     QTest::addColumn<int>("expectedReplacementCount");
     QTest::addColumn<Okteta::Address>("startIndex");
     QTest::addColumn<bool>("backwards");
+    QTest::addColumn<QVector<Kasten::ReplaceBehaviour>>("replies");
 
     const struct {
         QString name;
@@ -114,19 +162,81 @@ void ReplaceJobTest::testReplace_data()
         QTest::newRow(QString(data.name+QLatin1String("-forward-frombegin")).toLatin1().constData())
             << data.originalData << data.searchData
             << data.replaceData << data.expectedData << data.expectedReplacementCount
-            << Okteta::Address(0) << false;
+            << Okteta::Address(0) << false << QVector<Kasten::ReplaceBehaviour>();
         QTest::newRow(QString(data.name+QLatin1String("-backward-fromend")).toLatin1().constData())
             << data.originalData << data.searchData
             << data.replaceData << data.expectedData << data.expectedReplacementCount
-            << Okteta::Address(data.originalData.length()) << true;
+            << Okteta::Address(data.originalData.length()) << true << QVector<Kasten::ReplaceBehaviour>();
         QTest::newRow(QString(data.name+QLatin1String("-forward-frommiddle")).toLatin1().constData())
             << data.originalData << data.searchData
             << data.replaceData << data.expectedData << data.expectedReplacementCount
-            << Okteta::Address(4) << false;
+            << Okteta::Address(4) << false << QVector<Kasten::ReplaceBehaviour>();
         QTest::newRow(QString(data.name+QLatin1String("-backward-frommiddle")).toLatin1().constData())
             << data.originalData << data.searchData
             << data.replaceData << data.expectedData << data.expectedReplacementCount
-            << Okteta::Address(3) << true;
+            << Okteta::Address(3) << true << QVector<Kasten::ReplaceBehaviour>();
+    }
+
+    {
+        const QByteArray originalData = byteArrayFromLiteral("11111111");
+        const QByteArray expectedData = byteArrayFromLiteral("22222222");
+        const int expectedReplacementCount = 8;
+        const QByteArray searchData = byteArrayFromLiteral("1");
+        const QByteArray replaceData = byteArrayFromLiteral("2");
+
+        QTest::newRow("replies-forward-replaceall")
+            << originalData << searchData
+            << replaceData << expectedData << expectedReplacementCount
+            << Okteta::Address(0) << false
+            << QVector<Kasten::ReplaceBehaviour>{ Kasten::ReplaceAll };
+
+        QTest::newRow("replies-forward-replace8x")
+            << originalData << searchData
+            << replaceData << expectedData << expectedReplacementCount
+            << Okteta::Address(0) << false
+            << QVector<Kasten::ReplaceBehaviour>(8, Kasten::ReplaceCurrent);
+
+        QTest::newRow("replies-forward-replace3xreplaceall")
+            << originalData << searchData
+            << replaceData << expectedData << expectedReplacementCount
+            << Okteta::Address(0) << false
+            << QVector<Kasten::ReplaceBehaviour>{Kasten::ReplaceCurrent, Kasten::ReplaceCurrent, Kasten::ReplaceCurrent, Kasten::ReplaceAll };
+    }
+
+    {
+        QTest::newRow("replies-forward-replaceskipalternate")
+            << byteArrayFromLiteral("11111111") << byteArrayFromLiteral("1")
+            << byteArrayFromLiteral("2") << byteArrayFromLiteral("12121212") << 4
+            << Okteta::Address(0) << false
+            << QVector<Kasten::ReplaceBehaviour>{ Kasten::SkipCurrent, Kasten::ReplaceCurrent,
+                Kasten::SkipCurrent, Kasten::ReplaceCurrent, Kasten::SkipCurrent, Kasten::ReplaceCurrent,
+                Kasten::SkipCurrent, Kasten::ReplaceCurrent };
+    }
+
+    {
+        const QByteArray originalData = byteArrayFromLiteral("11111111");
+        const QByteArray expectedData = byteArrayFromLiteral("11111111");
+        const int expectedReplacementCount = 0;
+        const QByteArray searchData = byteArrayFromLiteral("1");
+        const QByteArray replaceData = byteArrayFromLiteral("2");
+
+        QTest::newRow("replies-forward-cancel")
+            << originalData << searchData
+            << replaceData << expectedData << expectedReplacementCount
+            << Okteta::Address(0) << false
+            << QVector<Kasten::ReplaceBehaviour>{ Kasten::CancelReplacing };
+
+        QTest::newRow("replies-forward-skip8x")
+            << originalData << searchData
+            << replaceData << expectedData << expectedReplacementCount
+            << Okteta::Address(0) << false
+            << QVector<Kasten::ReplaceBehaviour>(8, Kasten::SkipCurrent);
+
+        QTest::newRow("replies-forward-skip3xcancel")
+            << originalData << searchData
+            << replaceData << expectedData << expectedReplacementCount
+            << Okteta::Address(0) << false
+            << QVector<Kasten::ReplaceBehaviour>{Kasten::SkipCurrent, Kasten::SkipCurrent, Kasten::SkipCurrent, Kasten::CancelReplacing };
     }
 }
 
@@ -139,6 +249,9 @@ void ReplaceJobTest::testReplace()
     QFETCH(QByteArray, replaceData);
     QFETCH(QByteArray, expectedData);
     QFETCH(int, expectedReplacementCount);
+    QFETCH(QVector<Kasten::ReplaceBehaviour>, replies);
+
+    TestReplaceUserQueryAgent* queryAgent = replies.isEmpty() ? nullptr : new TestReplaceUserQueryAgent(replies);
 
     auto byteArray = new Okteta::PieceTableByteArrayModel(originalData);
     auto document = new Kasten::ByteArrayDocument(byteArray, QStringLiteral("init"));
@@ -157,11 +270,12 @@ void ReplaceJobTest::testReplace()
         replaceLastIndex =  byteArray->size() - 1;
     }
 
-    auto job = new Kasten::ReplaceJob(view, byteArray, nullptr);
+    auto job = new Kasten::ReplaceJob(view, byteArray, queryAgent);
     job->setSearchData(searchData);
     job->setReplaceData(replaceData);
     job->setRange(replaceFirstIndex, replaceLastIndex,
                   backwards ? Kasten::FindBackward : Kasten::FindForward);
+    job->setDoPrompt(queryAgent != nullptr);
 
     QSignalSpy finishedSpy(job, &Kasten::ReplaceJob::finished);
 
@@ -174,13 +288,18 @@ void ReplaceJobTest::testReplace()
 
     compare(byteArray, expectedData);
 
+    const int beforeWrap = queryAgent ? queryAgent->noOfReplacements() : 0;
     const QList<QVariant> arguments = finishedSpy.takeFirst();
     QCOMPARE(arguments.at(0).toBool(), true);
-    QCOMPARE(arguments.at(1).toInt(), expectedReplacementCount);
+    QCOMPARE(arguments.at(1).toInt() + beforeWrap, expectedReplacementCount);
 
     delete job;
     delete view;
     delete document;
+
+    delete queryAgent;
 }
 
 QTEST_MAIN(ReplaceJobTest)
+
+#include "replacejobtest.moc"
