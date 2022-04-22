79315e31662dccbf260f5ccbba80490105dcb78b
Friedrich W. H. Kossebau <kossebau@kde.org>
2022-04-22 15:26:40 +0200

    Use more explicit QChar constructors
    GIT_SILENT

--- core/codecs/hexadecimalbytecodec.cpp
+++ core/codecs/hexadecimalbytecodec.cpp
@@ -14,16 +14,24 @@
 namespace Okteta {
 
 static constexpr QChar upperCaseDigits[16] =
-{   // '0',   '1',   '2',   '3',   '4',   '5',   '6',   '7'   ,'8',   '9',
-    0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037, 0x0038, 0x0039,
-    // 'A',   'B',   'C',   'D',   'E',   'F'
-    0x0041, 0x0042, 0x0043, 0x0044, 0x0045, 0x0046
+{   // '0',        '1',           '2',           '3',
+    QChar(0x0030), QChar(0x0031), QChar(0x0032), QChar(0x0033),
+    // '4',        '5',           '6',           '7',
+    QChar(0x0034), QChar(0x0035), QChar(0x0036), QChar(0x0037),
+    // '8',        '9',           'A',           'B',
+    QChar(0x0038), QChar(0x0039), QChar(0x0041), QChar(0x0042),
+    // 'C',        'D',           'E',           'F'
+    QChar(0x0043), QChar(0x0044), QChar(0x0045), QChar(0x0046),
 };
 static constexpr QChar lowerCaseDigits[16] =
-{   // '0',   '1',   '2',   '3',   '4',   '5',   '6',   '7'   ,'8',   '9',
-    0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037, 0x0038, 0x0039,
-    // 'a',   'b',   'c',   'd',   'e',   'f'
-    0x0061, 0x0062, 0x0063, 0x0064, 0x0065, 0x0066
+{   // '0',        '1',           '2',           '3',
+    QChar(0x0030), QChar(0x0031), QChar(0x0032), QChar(0x0033),
+    // '4',        '5',           '6',           '7',
+    QChar(0x0034), QChar(0x0035), QChar(0x0036), QChar(0x0037),
+    // '8',        '9',           'a',           'b',
+    QChar(0x0038), QChar(0x0039), QChar(0x0061), QChar(0x0062),
+    // 'c',        'd',           'e',           'f'
+    QChar(0x0063), QChar(0x0064), QChar(0x0065), QChar(0x0066),
 };
 static constexpr Byte hexadecimalDigitsFilledLimit = 16;
 
