760983401e6ee40152d5e90db77cc06b9f164ed7
Laurent Montel <montel@kde.org>
2022-05-12 08:09:16 +0200

    Adapt build system for building against qt6

--- cmake/OktetaInternalMacros.cmake
+++ cmake/OktetaInternalMacros.cmake
@@ -135,7 +135,7 @@ macro(okteta_add_sublibrary _baseName)
     endforeach()
 
     foreach(_qrc ${OKTETA_ADD_SUBLIBRARY_QRC})
-        qt5_add_resources(_srcs "${_relativePath}${_qrc}")
+        qt_add_resources(_srcs "${_relativePath}${_qrc}")
     endforeach()
 
     target_sources(${_library_target} PRIVATE ${_srcs})
