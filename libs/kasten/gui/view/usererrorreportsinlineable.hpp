08680ef96ce9c7371704611b646bd8937eb7bd6a
Friedrich W. H. Kossebau <kossebau@kde.org>
2024-10-18 22:15:55 +0200

    Kasten: add UserErrorReportsInlineable

---
+++ libs/kasten/gui/view/usererrorreportsinlineable.hpp
@@ -0,0 +1,45 @@
+/*
+    This file is part of the Kasten Framework, made within the KDE community.
+
+    SPDX-FileCopyrightText: 2024 Friedrich W. H. Kossebau <kossebau@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
+#ifndef KASTEN_IF_USERERRORREPORTSINLINEABLE_HPP
+#define KASTEN_IF_USERERRORREPORTSINLINEABLE_HPP
+
+// Qt
+#include <QtPlugin>
+// Std
+#include <memory>
+
+namespace Kasten {
+
+class UserErrorReport;
+
+namespace If {
+
+class UserErrorReportsInlineable
+{
+public:
+    virtual ~UserErrorReportsInlineable();
+
+public: // actions
+    virtual void showErrorReport(UserErrorReport* errorReport) = 0;
+
+public: // get
+    virtual bool isErrorReportShown() const = 0;
+
+public: // signal
+    virtual void errorReportHidden() = 0;
+};
+
+inline UserErrorReportsInlineable::~UserErrorReportsInlineable() = default;
+
+}
+}
+
+Q_DECLARE_INTERFACE(Kasten::If::UserErrorReportsInlineable, "org.kde.kasten.if.usererrorreportsinlineable/1.0")
+
+#endif
