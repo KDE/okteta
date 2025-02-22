f0cc6c474862f8f6033c6ee6350c74882da71aec
Friedrich W. H. Kossebau <kossebau@kde.org>
2025-02-23 00:37:23 +0100

    ArrayChangeMetrics: note destructor & copy constructor being trivial

--- core/arraychangemetrics.hpp
+++ core/arraychangemetrics.hpp
@@ -45,10 +45,10 @@ public:
 
 public:
     ArrayChangeMetrics() = default;
-    ArrayChangeMetrics(const ArrayChangeMetrics&) = default;
+    ArrayChangeMetrics(const ArrayChangeMetrics&) = default; // trivial
     constexpr ArrayChangeMetrics(Type type, Address offset, qint32 secondArgument, qint32 thirdArgument);
 
-    ~ArrayChangeMetrics() = default;
+    ~ArrayChangeMetrics() = default; // trivial
 
     ArrayChangeMetrics& operator=(const ArrayChangeMetrics&) = default;
 
