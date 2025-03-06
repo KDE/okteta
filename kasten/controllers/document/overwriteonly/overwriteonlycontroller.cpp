KDE/okteta

commit c4bad033ac9ffc95ab26bc32babd360afb9ca646

Tools & controllers: check on new target model if any change in used model

kasten/controllers/view/bookmarks/bookmarkscontroller.cpp
@@ -90,6 +90,11 @@ BookmarksController::~BookmarksController() = default;
 
 void BookmarksController::setTargetModel(AbstractModel* model)
 {
+    ByteArrayView* const byteArrayView = model ? model->findBaseModel<ByteArrayView*>() : nullptr;
+    if (byteArrayView == mByteArrayView) {
+        return;
+    }
+
     if (mByteArrayView) {
         mByteArrayView->disconnect(this);
     }
@@ -97,8 +102,9 @@ void BookmarksController::setTargetModel(AbstractModel* model)
         mByteArray->disconnect(this);
     }
 
-    mByteArrayView = model ? model->findBaseModel<ByteArrayView*>() : nullptr;
+    mByteArrayView = byteArrayView;
 
+    // TODO; check if already same document used before
     ByteArrayDocument* document =
         mByteArrayView ? qobject_cast<ByteArrayDocument*>(mByteArrayView->baseModel()) : nullptr;
     mByteArray = document ? document->content() : nullptr;

kasten/controllers/view/overwritemode/overwritemodecontroller.cpp
@@ -38,11 +38,16 @@ OverwriteModeController::OverwriteModeController(KXMLGUIClient* guiClient)
 
 void OverwriteModeController::setTargetModel(AbstractModel* model)
 {
+    ByteArrayView* const byteArrayView = model ? model->findBaseModel<ByteArrayView*>() : nullptr;
+    if (byteArrayView == mByteArrayView) {
+        return;
+    }
+
     if (mByteArrayView) {
         mByteArrayView->disconnect(mSetOverWriteAction);
     }
 
-    mByteArrayView = model ? model->findBaseModel<ByteArrayView*>() : nullptr;
+    mByteArrayView = byteArrayView;
 
     if (mByteArrayView) {
         mSetOverWriteAction->setChecked(mByteArrayView->isOverwriteMode());

commit 21ec308e078b8292f7f5f581da7181fe3d641fd9

Tools & controllers: fix to properly initialize as disabled
Regression from c4bad033ac9ffc95ab26bc32babd360afb9ca646, which removed
the setup done by setting a nullptr model, due to being the default value.

kasten/controllers/document/overwriteonly/overwriteonlycontroller.cpp
@@ -22,10 +22,9 @@ OverwriteOnlyController::OverwriteOnlyController(KXMLGUIClient* guiClient)
 {
     mSetOverwriteOnlyAction = new KToggleAction(i18nc("@option:check", "Overwrite only"), this);
     connect(mSetOverwriteOnlyAction, &QAction::triggered, this, &OverwriteOnlyController::setOverwriteOnly);
+    mSetOverwriteOnlyAction->setEnabled(false);
 
     guiClient->actionCollection()->addAction(QStringLiteral("isoverwriteonly"), mSetOverwriteOnlyAction);
-
-    setTargetModel(nullptr);
 }
 
 void OverwriteOnlyController::setTargetModel(AbstractModel* model)

kasten/controllers/view/bookmarks/bookmarkscontroller.cpp
@@ -45,29 +45,34 @@ BookmarksController::BookmarksController(KXMLGUIClient* guiClient)
     KActionCollection* actionCollection = mGuiClient->actionCollection();
 
     mCreateAction = KStandardAction::addBookmark(this, &BookmarksController::createBookmark, this);
+    mCreateAction->setEnabled(false);
 
     mDeleteAction = new QAction(QIcon::fromTheme(QStringLiteral("bookmark-remove")),
                                 i18nc("@action:inmenu", "Remove Bookmark"), this);
     mDeleteAction->setObjectName(QStringLiteral("bookmark_remove"));
     connect(mDeleteAction, &QAction::triggered, this, &BookmarksController::deleteBookmark);
     actionCollection->setDefaultShortcut(mDeleteAction, Qt::CTRL | Qt::SHIFT | Qt::Key_B);
+    mDeleteAction->setEnabled(false);
 
     mDeleteAllAction = new QAction(QIcon::fromTheme(QStringLiteral("bookmark-remove")),
                                    i18nc("@action:inmenu", "Remove All Bookmarks"), this);
     mDeleteAllAction->setObjectName(QStringLiteral("bookmark_remove_all"));
     connect(mDeleteAllAction, &QAction::triggered, this, &BookmarksController::deleteAllBookmarks);
+    mDeleteAllAction->setEnabled(false);
 //     actionCollection->setDefaultShortcut( mDeleteAllAction, Qt::CTRL | Qt::Key_G );
 
     mGotoNextBookmarkAction = new QAction(QIcon::fromTheme(QStringLiteral("go-next")),
                                           i18nc("@action:inmenu", "Go to Next Bookmark"), this);
     mGotoNextBookmarkAction->setObjectName(QStringLiteral("bookmark_next"));
     connect(mGotoNextBookmarkAction, &QAction::triggered, this, &BookmarksController::gotoNextBookmark);
+    mGotoNextBookmarkAction->setEnabled(false);
     actionCollection->setDefaultShortcut(mGotoNextBookmarkAction, Qt::ALT | Qt::Key_Down);
 
     mGotoPreviousBookmarkAction = new QAction(QIcon::fromTheme(QStringLiteral("go-previous")),
                                               i18nc("@action:inmenu", "Go to Previous Bookmark"), this);
     mGotoPreviousBookmarkAction->setObjectName(QStringLiteral("bookmark_previous"));
     connect(mGotoPreviousBookmarkAction, &QAction::triggered, this, &BookmarksController::gotoPreviousBookmark);
+    mGotoPreviousBookmarkAction->setEnabled(false);
     actionCollection->setDefaultShortcut(mGotoPreviousBookmarkAction, Qt::ALT | Qt::Key_Up);
 
     mBookmarksActionGroup = new QActionGroup(this);   // TODO: do we use this only for the signal mapping?
@@ -82,8 +87,6 @@ BookmarksController::BookmarksController(KXMLGUIClient* guiClient)
         mGotoNextBookmarkAction,
         mGotoPreviousBookmarkAction
     });
-
-    setTargetModel(nullptr);
 }
 
 BookmarksController::~BookmarksController() = default;

kasten/controllers/view/overwritemode/overwritemodecontroller.cpp
@@ -30,10 +30,9 @@ OverwriteModeController::OverwriteModeController(KXMLGUIClient* guiClient)
     // TODO: or should we catch the signal from the view (needs to be added)
     actionCollection->setDefaultShortcut(mSetOverWriteAction, QKeySequence(Qt::Key_Insert));
     connect(mSetOverWriteAction, &KToggleAction::triggered, this, &OverwriteModeController::setOverWrite);
+    mSetOverWriteAction->setEnabled(false);
 
     actionCollection->addAction(QStringLiteral("set_overwrite"), mSetOverWriteAction);
-
-    setTargetModel(nullptr);
 }
 
 void OverwriteModeController::setTargetModel(AbstractModel* model)

kasten/controllers/view/print/printcontroller.cpp
@@ -23,9 +23,11 @@ PrintController::PrintController(KXMLGUIClient* guiClient)
     : mPrintTool(new PrintTool())
 {
     mPrintAction = KStandardAction::print(mPrintTool, &PrintTool::print, this);
+    mPrintAction->setEnabled(false);
     connect(mPrintTool, &PrintTool::viewChanged,
             mPrintAction, &QAction::setEnabled);
     mPrintPreviewAction = KStandardAction::printPreview(mPrintTool, &PrintTool::printPreview, this);
+    mPrintPreviewAction->setEnabled(false);
     connect(mPrintTool, &PrintTool::viewChanged,
             mPrintPreviewAction, &QAction::setEnabled);
 
@@ -33,9 +35,6 @@ PrintController::PrintController(KXMLGUIClient* guiClient)
         mPrintAction,
         mPrintPreviewAction,
     });
-
-
-    setTargetModel(nullptr);
 }
 
 PrintController::~PrintController()

kasten/controllers/view/viewconfig/viewconfigcontroller.cpp
@@ -46,6 +46,7 @@ ViewConfigController::ViewConfigController(KXMLGUIClient* guiClient)
     connect(mOffsetCodingAction, QOverload<int>::of(&KSelectAction::triggered),
 #endif
             this, &ViewConfigController::setOffsetCoding);
+    mOffsetCodingAction->setEnabled(false);
 
     // value valueCoding
     mCodingAction = new KSelectAction(i18nc("@title:menu", "&Value Coding"), this);
@@ -66,6 +67,7 @@ ViewConfigController::ViewConfigController(KXMLGUIClient* guiClient)
     connect(mCodingAction, QOverload<int>::of(&KSelectAction::triggered),
 #endif
             this, &ViewConfigController::setValueCoding);
+    mCodingAction->setEnabled(false);
 
     // char valueCoding
     mEncodingAction = new KSelectAction(i18nc("@title:menu", "&Char Coding"), this);
@@ -77,23 +79,27 @@ ViewConfigController::ViewConfigController(KXMLGUIClient* guiClient)
     connect(mEncodingAction, QOverload<int>::of(&KSelectAction::triggered),
 #endif
             this, &ViewConfigController::setCharCoding);
+    mEncodingAction->setEnabled(false);
 
     mShowsNonprintingAction = new KToggleAction(i18nc("@option:check", "Show &Non-printing Chars"), this);
     mShowsNonprintingAction->setObjectName(QStringLiteral("view_showsnonprinting"));
     connect(mShowsNonprintingAction, &KToggleAction::triggered,
             this, &ViewConfigController::setShowsNonprinting);
+    mShowsNonprintingAction->setEnabled(false);
 
     // bytes per line
     mSetBytesPerLineAction = new QAction(i18nc("@action:inmenu", "Set Bytes per Line..."), this);
     mSetBytesPerLineAction->setObjectName(QStringLiteral("view_bytesperline"));
     connect(mSetBytesPerLineAction, &QAction::triggered,
             this, &ViewConfigController::showBytesPerLineDialog);
+    mSetBytesPerLineAction->setEnabled(false);
 
     // byte groups size
     mSetBytesPerGroupAction = new QAction(i18nc("@action:inmenu", "Set Bytes per Group..."), this);
     mSetBytesPerGroupAction->setObjectName(QStringLiteral("view_bytespergroup"));
     connect(mSetBytesPerGroupAction, &QAction::triggered,
             this, &ViewConfigController::showBytesPerGroupDialog);
+    mSetBytesPerGroupAction->setEnabled(false);
 
     // resize style
     mResizeStyleAction = new KSelectAction(i18nc("@title:menu", "&Dynamic Layout"), this);
@@ -112,12 +118,14 @@ ViewConfigController::ViewConfigController(KXMLGUIClient* guiClient)
     connect(mResizeStyleAction, QOverload<int>::of(&KSelectAction::triggered),
 #endif
             this, &ViewConfigController::setLayoutStyle);
+    mResizeStyleAction->setEnabled(false);
 
     mShowOffsetColumnAction = new KToggleAction(i18nc("@option:check", "Show &Line Offset"), this);
     mShowOffsetColumnAction->setObjectName(QStringLiteral("view_lineoffset"));
     actionCollection->setDefaultShortcut(mShowOffsetColumnAction, Qt::Key_F11);
     connect(mShowOffsetColumnAction, &KToggleAction::triggered,
             this, &ViewConfigController::toggleOffsetColumn);
+    mShowOffsetColumnAction->setEnabled(false);
 
     // show buffer columns
     mToggleColumnsAction = new KSelectAction(i18nc("@title:menu", "&Show Values or Chars"), this);
@@ -133,6 +141,7 @@ ViewConfigController::ViewConfigController(KXMLGUIClient* guiClient)
     connect(mToggleColumnsAction, QOverload<int>::of(&KSelectAction::triggered),
 #endif
             this, &ViewConfigController::toggleValueCharColumns);
+    mToggleColumnsAction->setEnabled(false);
 
     actionCollection->addActions({
         mCodingAction,
@@ -145,8 +154,6 @@ ViewConfigController::ViewConfigController(KXMLGUIClient* guiClient)
         mOffsetCodingAction,
         mToggleColumnsAction,
     });
-
-    setTargetModel(nullptr);
 }
 
 void ViewConfigController::setTargetModel(AbstractModel* model)

kasten/controllers/view/viewcontextmenu/viewcontextmenucontroller.cpp
@@ -21,7 +21,6 @@ namespace Kasten {
 ViewContextMenuController::ViewContextMenuController(KXMLGUIClient* guiClient)
     : mGuiClient(guiClient)
 {
-    setTargetModel(nullptr);
 }
 
 void ViewContextMenuController::setTargetModel(AbstractModel* model)

kasten/controllers/view/viewmode/viewmodecontroller.cpp
@@ -33,10 +33,9 @@ ViewModeController::ViewModeController(KXMLGUIClient* guiClient)
     connect(mViewModeAction, QOverload<int>::of(&KSelectAction::triggered),
 #endif
             this, &ViewModeController::setViewMode);
+    mViewModeAction->setEnabled(false);
 
     guiClient->actionCollection()->addAction(QStringLiteral("viewmode"), mViewModeAction);
-
-    setTargetModel(nullptr);
 }
 
 void ViewModeController::setTargetModel(AbstractModel* model)

kasten/controllers/view/viewprofiles/viewprofilecontroller.cpp
@@ -40,6 +40,7 @@ ViewProfileController::ViewProfileController(ByteArrayViewProfileManager* viewPr
 #else
     mViewProfileActionMenu->setDelayed(false);
 #endif
+    mViewProfileActionMenu->setEnabled(false);
 
     mCreateNewAction =
         new QAction(QIcon::fromTheme(QStringLiteral("document-new")),
@@ -47,6 +48,7 @@ ViewProfileController::ViewProfileController(ByteArrayViewProfileManager* viewPr
                           "Create New..."),
                     this);
     connect(mCreateNewAction, &QAction::triggered, this, &ViewProfileController::onCreateNewActionTriggered);
+    mCreateNewAction->setEnabled(false);
 
     mSaveChangesAction =
         new QAction(QIcon::fromTheme(QStringLiteral("document-save")),
@@ -54,6 +56,7 @@ ViewProfileController::ViewProfileController(ByteArrayViewProfileManager* viewPr
                           "Save Changes"),
                     this);
     connect(mSaveChangesAction, &QAction::triggered, this, &ViewProfileController::onSaveChangesActionTriggered);
+    mSaveChangesAction->setEnabled(false);
 
     mResetChangesAction =
         new QAction(QIcon::fromTheme(QStringLiteral("document-revert")),
@@ -61,6 +64,7 @@ ViewProfileController::ViewProfileController(ByteArrayViewProfileManager* viewPr
                           "Reset Changes"),
                     this);
     connect(mResetChangesAction, &QAction::triggered, this, &ViewProfileController::onResetChangesActionTriggered);
+    mResetChangesAction->setEnabled(false);
 
     mViewProfileActionMenu->addAction(mCreateNewAction);
     mViewProfileActionMenu->addSeparator();
@@ -80,8 +84,6 @@ ViewProfileController::ViewProfileController(ByteArrayViewProfileManager* viewPr
             this, &ViewProfileController::onViewProfilesChanged);
 
     onViewProfilesChanged();
-
-    setTargetModel(nullptr);
 }
 
 void ViewProfileController::setTargetModel(AbstractModel* model)

kasten/controllers/view/viewstatus/viewstatuscontroller.cpp
@@ -31,9 +31,11 @@ ViewStatusController::ViewStatusController(StatusBar* statusBar)
     mPrintFunction = Okteta::OffsetFormat::printFunction(Okteta::OffsetFormat::Hexadecimal);
 
     mOffsetLabel = new QLabel(statusBar);
+    mOffsetLabel->setEnabled(false);
     statusBar->addWidget(mOffsetLabel);
 
     mSelectionLabel = new QLabel(statusBar);
+    mSelectionLabel->setEnabled(false);
     statusBar->addWidget(mSelectionLabel);
 
     const QString insertModeText = i18nc("@info:status short for: Insert mode",    "INS");
@@ -44,6 +46,7 @@ ViewStatusController::ViewStatusController(StatusBar* statusBar)
     mOverwriteModeToggleButton->setCheckedState(overwriteModeText, overwriteModeTooltip);
     statusBar->addWidget(mOverwriteModeToggleButton);
     connect(mOverwriteModeToggleButton, &ToggleButton::clicked, this, &ViewStatusController::setOverwriteMode);
+    mOverwriteModeToggleButton->setEnabled(false);
 
     mValueCodingComboBox = new KComboBox(statusBar);
     const QStringList list {
@@ -57,6 +60,7 @@ ViewStatusController::ViewStatusController(StatusBar* statusBar)
         i18nc("@info:tooltip", "Coding of the value interpretation in the current view."));
     connect(mValueCodingComboBox, QOverload<int>::of(&KComboBox::activated),
             this, &ViewStatusController::setValueCoding);
+    mValueCodingComboBox->setEnabled(false);
     statusBar->addWidget(mValueCodingComboBox);
 
     mCharCodingComboBox = new KComboBox(statusBar);
@@ -65,11 +69,12 @@ ViewStatusController::ViewStatusController(StatusBar* statusBar)
         i18nc("@info:tooltip", "Encoding in the character column of the current view."));
     connect(mCharCodingComboBox, QOverload<int>::of(&KComboBox::activated),
             this, &ViewStatusController::setCharCoding);
+    mCharCodingComboBox->setEnabled(false);
     statusBar->addWidget(mCharCodingComboBox);
 
     fixWidths(Okteta::OffsetFormat::Hexadecimal);
 
-    setTargetModel(nullptr);
+    clearDisplay();
 }
 
 // the letter C can be very wide, that is why with proportional fonts there seems too much space used, but isn't
@@ -165,11 +170,7 @@ void ViewStatusController::setTargetModel(AbstractModel* model)
         connect(mByteArrayView, &ByteArrayView::charCodecChanged,
                 this, &ViewStatusController::onCharCodecChanged);
     } else {
-        mOffsetLabel->setText(i18nc("@info:status offset value not available", "Offset: -"));
-        mSelectionLabel->setText(i18nc("@info:status offset value not available", "Selection: -"));
-        mOverwriteModeToggleButton->setChecked(false);
-        mValueCodingComboBox->setCurrentIndex(0);
-        mCharCodingComboBox->setCurrentIndex(0);
+        clearDisplay();
     }
 
     mOffsetLabel->setEnabled(hasView);
@@ -194,6 +195,15 @@ void ViewStatusController::setCharCoding(int charCoding)
 {
     mByteArrayView->setCharCoding(Okteta::CharCodec::codecNames()[charCoding]);
     mByteArrayView->setFocus();
+}
+
+void ViewStatusController::clearDisplay()
+{
+    mOffsetLabel->setText(i18nc("@info:status offset value not available", "Offset: -"));
+    mSelectionLabel->setText(i18nc("@info:status offset value not available", "Selection: -"));
+    mOverwriteModeToggleButton->setChecked(false);
+    mValueCodingComboBox->setCurrentIndex(0);
+    mCharCodingComboBox->setCurrentIndex(0);
 }
 
 void ViewStatusController::onCursorPositionChanged(Okteta::Address offset)

kasten/controllers/view/viewstatus/viewstatuscontroller.hpp
@@ -39,6 +39,7 @@ public: // AbstractXmlGuiController API
 
 private:
     void fixWidths(int offsetCoding);
+    void clearDisplay();
 
 private Q_SLOTS:
     void setOverwriteMode(bool overwrite);

libs/kasten/controllers/document/modified/modifiedbarcontroller.cpp
@@ -24,12 +24,15 @@ namespace Kasten {
 ModifiedBarController::ModifiedBarController(StatusBar* statusBar)
 {
     mLocalStateLabel = new IconLabel(statusBar);
+    mLocalStateLabel->setEnabled(false);
     statusBar->addWidget(mLocalStateLabel);
 
     mRemoteStateLabel = new IconLabel(statusBar);
+    mRemoteStateLabel->setEnabled(false);
     statusBar->addWidget(mRemoteStateLabel);
 
-    setTargetModel(nullptr);
+    onLocalSyncStateChanged(LocalInSync);
+    onRemoteSyncStateChanged(RemoteInSync);
 }
 
 void ModifiedBarController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/document/readonly/readonlybarcontroller.cpp
@@ -27,8 +27,8 @@ ReadOnlyBarController::ReadOnlyBarController(StatusBar* statusBar)
     statusBar->addWidget(mReadOnlyButton);
     connect(mReadOnlyButton, &QAbstractButton::clicked,
             this, &ReadOnlyBarController::setReadOnly);
-
-    setTargetModel(nullptr);
+    mReadOnlyButton->setChecked(false);
+    mReadOnlyButton->setEnabled(false);
 }
 
 void ReadOnlyBarController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/document/readonly/readonlycontroller.cpp
@@ -29,10 +29,9 @@ ReadOnlyController::ReadOnlyController(KXMLGUIClient* guiClient)
     mSetReadOnlyAction->setCheckedState(checkedState);
     connect(mSetReadOnlyAction, &QAction::triggered,
             this, &ReadOnlyController::setReadOnly);
+    mSetReadOnlyAction->setEnabled(false);
 
     guiClient->actionCollection()->addAction(QStringLiteral("isreadonly"), mSetReadOnlyAction);
-
-    setTargetModel(nullptr);
 }
 
 void ReadOnlyController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/io/clipboard/clipboardcontroller.cpp
@@ -28,8 +28,11 @@ namespace Kasten {
 ClipboardController::ClipboardController(KXMLGUIClient* guiClient)
 {
     mCutAction =   KStandardAction::cut(  this, &ClipboardController::cut,   this);
+    mCutAction->setEnabled(false);
     mCopyAction =  KStandardAction::copy( this, &ClipboardController::copy,  this);
+    mCopyAction->setEnabled(false);
     mPasteAction = KStandardAction::paste(this, &ClipboardController::paste, this);
+    mPasteAction->setEnabled(false);
 
     guiClient->actionCollection()->addActions({
         mCutAction,
@@ -39,8 +42,6 @@ ClipboardController::ClipboardController(KXMLGUIClient* guiClient)
 
     connect(QApplication::clipboard(), &QClipboard::dataChanged,
             this, &ClipboardController::onClipboardDataChanged);
-
-    setTargetModel(nullptr);
 }
 
 void ClipboardController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/io/copyas/copyascontroller.cpp
@@ -48,7 +48,7 @@ CopyAsController::CopyAsController(ModelCodecViewManager* modelCodecViewManager,
 
     guiClient->actionCollection()->addAction(QStringLiteral("copy_as"), mCopyAsSelectAction);
 
-    setTargetModel(nullptr);
+    updateActions();
 }
 
 void CopyAsController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/io/export/exportcontroller.cpp
@@ -44,7 +44,7 @@ ExportController::ExportController(ModelCodecViewManager* modelCodecViewManager,
 
     guiClient->actionCollection()->addAction(QStringLiteral("export"), mExportSelectAction);
 
-    setTargetModel(nullptr);
+    updateActions();
 }
 
 void ExportController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/io/insert/insertcontroller.cpp
@@ -45,7 +45,7 @@ InsertController::InsertController(ModelCodecViewManager* modelCodecViewManager,
     // TODO: find better id
     guiClient->actionCollection()->addAction(QStringLiteral("insert"), mInsertSelectAction);
 
-    setTargetModel(nullptr);
+    updateActions();
 }
 
 void InsertController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/io/setremote/setremotecontroller.cpp
@@ -24,10 +24,9 @@ SetRemoteController::SetRemoteController(DocumentSyncManager* syncManager, KXMLG
     : mSyncManager(syncManager)
 {
     mSaveAsAction = KStandardAction::saveAs(this, &SetRemoteController::saveAs, this);
+    mSaveAsAction->setEnabled(false);
 
     guiClient->actionCollection()->addAction(mSaveAsAction->objectName(), mSaveAsAction);
-
-    setTargetModel(nullptr);
 }
 
 void SetRemoteController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/io/synchronize/synchronizecontroller.cpp
@@ -28,17 +28,17 @@ SynchronizeController::SynchronizeController(DocumentSyncManager* syncManager, K
     KActionCollection* actionCollection = guiClient->actionCollection();
 
     mSaveAction = KStandardAction::save(this, &SynchronizeController::save, this);
+    mSaveAction->setEnabled(false);
 
     mReloadAction = new QAction(QIcon::fromTheme(QStringLiteral("view-refresh")),
                                 i18nc("@action:inmenu", "Reloa&d"), this);
     actionCollection->setDefaultShortcuts(mReloadAction, KStandardShortcut::reload());
     connect(mReloadAction, &QAction::triggered,
             this, &SynchronizeController::reload);
+    mReloadAction->setEnabled(false);
 
     actionCollection->addAction(mSaveAction->objectName(), mSaveAction);
     actionCollection->addAction(QStringLiteral("file_reload"), mReloadAction);
-
-    setTargetModel(nullptr);
 }
 
 void SynchronizeController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/view/select/selectcontroller.cpp
@@ -23,13 +23,13 @@ namespace Kasten {
 SelectController::SelectController(KXMLGUIClient* guiClient)
 {
     mSelectAllAction = KStandardAction::selectAll(this, &SelectController::selectAll, this);
+    mSelectAllAction->setEnabled(false);
     mDeselectAction =  KStandardAction::deselect( this, &SelectController::unselect,  this);
+    mDeselectAction->setEnabled(false);
 
     KActionCollection* actionCollection = guiClient->actionCollection();
     actionCollection->addAction(mSelectAllAction->objectName(), mSelectAllAction);
     actionCollection->addAction(mDeselectAction->objectName(), mDeselectAction);
-
-    setTargetModel(nullptr);
 }
 
 void SelectController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/view/version/versioncontroller.cpp
@@ -30,6 +30,7 @@ VersionController::VersionController(KXMLGUIClient* guiClient)
     KActionCollection* actionCollection = guiClient->actionCollection();
 
     mSetToOlderVersionAction = new KToolBarPopupAction(QIcon::fromTheme(QStringLiteral("edit-undo")), i18nc("@action:inmenu", "Undo"), this);
+    mSetToOlderVersionAction->setEnabled(false);
     actionCollection->addAction(QStringLiteral("edit_undo"), mSetToOlderVersionAction);
     actionCollection->setDefaultShortcuts(mSetToOlderVersionAction, KStandardShortcut::undo());
 
@@ -41,6 +42,7 @@ VersionController::VersionController(KXMLGUIClient* guiClient)
             this, &VersionController::onOlderVersionMenuTriggered);
 
     mSetToNewerVersionAction = new KToolBarPopupAction(QIcon::fromTheme(QStringLiteral("edit-redo")), i18nc("@action:inmenu", "Redo"), this);
+    mSetToNewerVersionAction->setEnabled(false);
     actionCollection->addAction(QStringLiteral("edit_redo"), mSetToNewerVersionAction);
     actionCollection->setDefaultShortcuts(mSetToNewerVersionAction, KStandardShortcut::redo());
 
@@ -50,8 +52,6 @@ VersionController::VersionController(KXMLGUIClient* guiClient)
             this, &VersionController::onNewerVersionMenuAboutToShow);
     connect(mSetToNewerVersionAction->menu(), &QMenu::triggered,
             this, &VersionController::onNewerVersionMenuTriggered);
-
-    setTargetModel(nullptr);
 }
 
 void VersionController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/view/zoom/zoombarcontroller.cpp
@@ -19,8 +19,6 @@ ZoomBarController::ZoomBarController(StatusBar* statusBar)
 {
     mZoomSlider = new ZoomSlider(statusBar);
     statusBar->addWidget(mZoomSlider);
-
-    setTargetModel(nullptr);
 }
 
 void ZoomBarController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/view/zoom/zoomcontroller.cpp
@@ -29,8 +29,11 @@ static constexpr int DefaultZoomControllerZoomInLevelsSize = 50;
 ZoomController::ZoomController(KXMLGUIClient* guiClient)
 {
     mZoomInAction = KStandardAction::zoomIn(  this, &ZoomController::zoomIn,  this);
+    mZoomInAction->setEnabled(false);
     mZoomOutAction = KStandardAction::zoomOut(this, &ZoomController::zoomOut, this);
+    mZoomOutAction->setEnabled(false);
     mZoomNormalAction = KStandardAction::actualSize(this, &ZoomController::zoomNormal, this);
+    mZoomNormalAction->setEnabled(false);
 
     guiClient->actionCollection()->addActions({
         mZoomInAction,
@@ -68,7 +71,6 @@ ZoomController::ZoomController(KXMLGUIClient* guiClient)
     FitToSizeAction = new KAction(i18n("&Fit to Size"), ActionCollection, "fit_to_size");
     connect(FitToSizeAction, SIGNAL(triggered(bool)), SLOT(fitToSize()));
 #endif
-    setTargetModel(nullptr);
 }
 
 void ZoomController::setTargetModel(AbstractModel* model)

libs/kasten/controllers/view/zoom/zoomslider.cpp
@@ -41,12 +41,14 @@ ZoomSlider::ZoomSlider(QWidget* parent)
     auto* zoomNormalButton = new QToolButton(this);
     m_zoomNormalAction = KStandardAction::actualSize(this, &ZoomSlider::zoomNormal, this);
     m_zoomNormalAction->setShortcut(QKeySequence()); // unset shortcut, here no integration into main menu
+    m_zoomNormalAction->setEnabled(false);
     zoomNormalButton->setDefaultAction(m_zoomNormalAction);
     zoomNormalButton->setAutoRaise(true);
 
     auto* zoomOutButton = new QToolButton(this);
     m_zoomOutAction = KStandardAction::zoomOut(this, &ZoomSlider::zoomOut, this);
     m_zoomOutAction->setShortcut(QKeySequence()); // unset shortcut, here no integration into main menu
+    m_zoomOutAction->setEnabled(false);
     zoomOutButton->setDefaultAction(m_zoomOutAction);
     zoomOutButton->setAutoRaise(true);
 
@@ -54,10 +56,12 @@ ZoomSlider::ZoomSlider(QWidget* parent)
     mSlider = new QSlider(Qt::Horizontal, this);
     mSlider->setSingleStep(1);   // mZoomControl->zoomLevelSingleStep()?
     mSlider->setPageStep(5);   // mZoomControl->zoomLevelPageStep()?
+    mSlider->setEnabled(false);
 
     auto* zoomInButton = new QToolButton(this);
     m_zoomInAction = KStandardAction::zoomIn(this, &ZoomSlider::zoomIn,  this);
     m_zoomInAction->setShortcut(QKeySequence()); // unset shortcut, here no integration into main menu
+    m_zoomInAction->setEnabled(false);
     zoomInButton->setDefaultAction(m_zoomInAction);
     zoomInButton->setAutoRaise(true);
 
@@ -75,8 +79,7 @@ ZoomSlider::ZoomSlider(QWidget* parent)
             this, &ZoomSlider::onSliderMoved);
 
     setFixedWidth(ZoomSliderWidth);
-
-    setTargetModel(nullptr);
+    clearDisplay();
 }
 
 ZoomSlider::~ZoomSlider() = default;
@@ -121,12 +124,17 @@ void ZoomSlider::setTargetModel(AbstractModel* model)
         m_zoomNormalAction->setEnabled(false);
         m_zoomOutAction->setEnabled(false);
         m_zoomInAction->setEnabled(false);
-        // put slider in the middle
-        mSlider->setRange(-DefaultZoomSliderZoomInLevelsSize, DefaultZoomSliderZoomOutLevelsSize);
-        mSlider->setValue(0);
+        clearDisplay();
     }
 
     mSlider->setEnabled(hasView);
+}
+
+void ZoomSlider::clearDisplay()
+{
+    // put slider in the middle
+    mSlider->setRange(-DefaultZoomSliderZoomInLevelsSize, DefaultZoomSliderZoomOutLevelsSize);
+    mSlider->setValue(0);
 }
 
 void ZoomSlider::updateToolTip(int sliderValue)

libs/kasten/controllers/view/zoom/zoomslider.hpp
@@ -36,6 +36,7 @@ public:
 
 private:
     void updateToolTip(int sliderValue);
+    void clearDisplay();
 
 private Q_SLOTS: // action slots
     void zoomIn();

libs/kasten/controllers/viewsystem/close/closecontroller.cpp
@@ -23,10 +23,9 @@ CloseController::CloseController(ViewManager* viewManager, KXMLGUIClient* guiCli
     : mViewManager(viewManager)
 {
     mCloseAction = KStandardAction::close(this, &CloseController::close, this);
+    mCloseAction->setEnabled(false);
 
     guiClient->actionCollection()->addAction(mCloseAction->objectName(), mCloseAction);
-
-    setTargetModel(0);
 }
 
 void CloseController::setTargetModel(AbstractModel* model)