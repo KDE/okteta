b581d29f7be5e9f5821f319d7e62c6b734de771e
Friedrich W. H. Kossebau <kossebau@kde.org>
2026-08-11 20:36:13 +0200

    AbstractByteArrayView: properly mark QContextMenuEvent as accepted

--- gui/abstractbytearrayview_p.cpp
+++ gui/abstractbytearrayview_p.cpp
@@ -1471,6 +1471,8 @@ void AbstractByteArrayViewPrivate::contextMenuEvent(QContextMenuEvent* contextMe
     menu->setAttribute(Qt::WA_DeleteOnClose);
 
     menu->popup(contextMenuEvent->globalPos());
+
+    contextMenuEvent->accept();
 }
 
 void AbstractByteArrayViewPrivate::timerEvent(QTimerEvent* timerEvent)
